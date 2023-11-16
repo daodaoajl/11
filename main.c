b27b02475d4487bc0f085e951c233c73b3e6bdb1
daodaoajl <144930715+daodaoajl@users.noreply.github.com>
2023-11-16 15:40:35 +0900

    3

--- main.c
+++ main.c
@@ -1,15 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//call by value
+
+/*void swap (int x, int y)
+{
+    int temp;
+    temp = x;
+    x = y;
+    y = temp;
+}
+
 void main(void)
 {
-  //int i = 4; //result : 4, 4, 4
-  int i = 300; //
+  int a = 3;
+  int b = 5;
+  swap(a,b);
+  printf("a:%i,b:%i\n",a,b);
+  
+  system("PAUSE");	
+  return 0;
+}*/
+//결과  a : 3, b : 5
 
-  int*pi = &i;
-  char*pc = &i;
+//call by reterence
 
-  printf("%i, %i, %i\n", i, *pi,*pc);
+void swap (int *x, int *y)
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void main(void)
+{
+  int a = 3;
+  int b = 5;
+  swap(&a,&b);
+  printf("a:%i,b:%i\n",a,b);
+
   system("PAUSE");	
   return 0;
 }
+// 결과 a : 5, b : 3  
